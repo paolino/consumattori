c1d4601ab49fa06c182cd9ec3adf804ba42bd0f5
Paolo Veronelli <paolo.veronelli@gmail.com>
2012-06-08 20:35:38 +0200

    operatore completo

--- Algorythm2.hs
+++ Algorythm2.hs
@@ -1,18 +1,21 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving, TypeFamilies, DataKinds, GADTs, BangPatterns #-}
-import Data.Array
-import Data.Ix
-import Data.Ratio
-import Control.Arrow
-import System.Random
-import System.Random.Shuffle
-import Data.Traversable
-import Control.Monad
-import Data.Foldable hiding (concatMap)
+{-# LANGUAGE GeneralizedNewtypeDeriving, TypeFamilies, TypeOperators, DataKinds, GADTs, BangPatterns, NoMonomorphismRestriction #-}
+import Prelude hiding (foldr, sum, concatMap)
+import Data.List (sortBy)
+import Data.Ord (comparing)
+import Data.Array (listArray, array, Array, elems, bounds, (!))
+import Data.Ix (Ix)
+import Data.Ratio (Rational)
+import Control.Arrow ((&&&), second)
+import System.Random (StdGen, split)
+import System.Random.Shuffle (shuffle')
+import Data.Traversable (mapAccumL)
+import Control.Monad (ap)
+import Data.Foldable (foldr, sum, concatMap)
 
 import Genetica2 
 
 newtype Utente = Utente Int deriving (Ord,Ix,Eq, Num, Enum)
-newtype Prodotto = Prodotto Int deriving (Ord,Ix,Eq)
+newtype Prodotto = Prodotto Int deriving (Ord,Ix,Eq, Num)
 
 newtype Quantità = Quantità Rational deriving (Num, Ord, Eq)
 
@@ -39,23 +42,27 @@ trasfer dqs = trasf where
 	trasf x@(Negativo, _) 	y@(Negativo, _)	= fake Positivo  x y 
 	trasf x@(Positivo, _) 	y@(Positivo, _) = fake Negativo  x y 
 
-mix :: [Quantità] -> [(Interesse,Quantità)] -> [(Interesse,Quantità)] -> [(Quantità,Quantità)]
-mix = zipWith3 trasfer
-
 newtype Rank = Rank Int deriving (Ord, Eq)
 
-newtype Matrice q = Matrice (Array Utente [q])
-type Ranks  = Array Utente Rank
-type Quanti = [Quantità]
+newtype Matrice q = Matrice (DatiUtente (DatiProdotto q))
 
-newtype Bounds = Bounds (Utente,Prodotto)
+zipMatrice f = zipArray (zipArray f)
+
+zipArray :: Ix i => (a -> b -> c) -> Array i a -> Array i b -> Array i c
+zipArray f a = snd . mapAccumL (\(x:xs) y -> (xs, f x y)) (elems a)
+
+type DatiUtente q = Array Utente q
+type DatiProdotto q = Array Prodotto q
+
+type Ranks  = DatiUtente Rank
+type Quanti = DatiProdotto Quantità
 
 newtype Rapporto = Rapporto Rational deriving (Ord, Eq, Num)
 
 type instance Valore (Matrice Quantità) = Rapporto
 
-valutazione :: Matrice Quantità -> Rapporto
-valutazione = undefined
+newGen :: StdGen -> StdGen
+newGen = fst . split
 
 flex :: [Rapporto] -> Bool
 flex [] = False
@@ -63,54 +70,51 @@ flex [_] = False
 flex [_,_] = False
 flex (vm:vx:vM:_) = vx >= vm && vx >= vM
 
+data Giudizio = Giudizio [Prodotto] (Quantità -> Bool)
 
-opera :: Bounds -> StdGen -> Quanti -> Ranks -> Matrice Interesse -> Matrice Quantità -> Matrice Quantità
-opera b@(Bounds (u,_)) s qs rs mi mq = let 
-	ijs = zip `ap` drop (fromEnum u `div` 2) $ shuffle' [1..u] (fromEnum u) s
-	in svolgiOperazione  b  rs qs (zipMatrice mi mq) ijs
-	{-
-	zippa matrici
-	prendi coppie di colonne
--}
+valutazione :: DatiUtente [Giudizio] ->  Matrice Quantità -> Rapporto
+valutazione vg (Matrice mq) = foldr f 0 (zipArray g vg mq) where
+	g :: [Giudizio] -> DatiProdotto Quantità -> [Bool]
+	g gs aq = map (\(Giudizio ps fq) -> fq . sum . map (aq !) $ ps) gs
+	f :: [Bool] -> Rapporto -> Rapporto 
+	f bs (Rapporto r) = let (rs,ws) = foldr (\x (rs,ws) -> if x then (rs + 1, ws) else (rs,ws + 1)) (1,1) bs 
+		in Rapporto $ r + rs / ws
 
-zipMatrice :: Matrice a -> Matrice b -> Matrice (a,b)
-zipMatrice (Matrice m1) (Matrice m2) = Matrice . snd $ mapAccumL (\(x:xs) y -> (xs,zip x y)) (toList m1) m2
-
-
-svolgiOperazione :: Bounds -> Ranks -> Quanti -> Matrice (Interesse,Quantità) -> [(Utente,Utente)] -> Matrice Quantità
-svolgiOperazione (Bounds (u,_)) rs qs (Matrice m) ijs = Matrice $ array (1,u) $ concatMap coppie ijs
-	where coppie (i,j) = let
-		(i',j') = if rs ! i <= rs ! j then (i,j) else (j,i)
-		(mi',mi'') = unzip $ mix qs (m ! i') (m ! j')
-		in [(i',mi'), (j',mi'')]
-
-
-
-
+ 
 newtype Limite = Limite Int deriving (Num,Ord,Eq)
 
-modifica :: Bounds -> Limite -> Quanti -> Ranks -> Matrice Interesse -> StdGen -> Matrice Quantità -> (Rapporto, Matrice Quantità)
-modifica b@(Bounds (u,_)) l qs rs mi s mq =  modifica' s (Limite 0) [valutazione mq] mq where
-	modifica' :: StdGen -> Limite -> [Rapporto] -> Matrice Quantità -> (Rapporto, Matrice Quantità)
-	modifica' s !n pvs  mq@(Matrice aq) = let
-		(s',_) = split s 
-		mq' = opera b s qs rs mi mq
-		nvs = (:pvs) $ valutazione mq'
-		in case flex nvs || n >= l of
-			True -> (head nvs, mq)
-			False -> modifica' s' (n + 1) nvs mq'
+newtype Giudizi = Giudizi (DatiUtente [Giudizio])
 
+operatore 	::   	Limite 	-- ^ numero massimo di iterazioni a caccia del massimo
+		-> 	Quanti  -- ^ quantità unitarie di trasferimento
+		-> 	Ranks 	-- ^ classifica di biasimo
+		-> 	Giudizi -- ^ preferenze utenti
+		-> 	Matrice Interesse -- ^ descrizione operatore
+		-> 	StdGen 	-- ^ seme random iniziale
+		-> 	Elemento (Variazioni Soluzioni) (Matrice Quantità) -- ^ operatore di primo livello
+operatore l qs rs (Giudizi gs) (Matrice mi) = Operatore . f  
+	where	(1,u) = bounds mi
+		(1,p) = bounds qs
+		f s (Soluzione mq) = (Operatore $ f (newGen s), second soluzione nmq) 
+			where	vmq = valutazione gs mq
+				nmq = corri s (Limite 0) [vmq] $ mq 
+				corri :: StdGen -> Limite -> [Rapporto] -> Matrice Quantità -> (Rapporto,Matrice Quantità)
+				corri s !n pvs mq@(Matrice aq) = if	flex nvs || n >= l 
+							then 	(nvs !! 1, mq) 
+							else  	corri (newGen s) (n + 1) nvs nmq 
+					where	ijs 	= zip `ap` drop (fromEnum u `div` 2) $ shuffle' [1..u] (fromEnum u) s
+						nmq 	= Matrice $ array (1,u) $ concatMap coppie ijs 
+						nvs 	= valutazione gs nmq : pvs
+						miq 	= zipArray (zipArray (,)) mi aq
+						coppie (i,j) = [(i',listArray (1,p) mi'), (j',listArray (bounds qs) mi'')] 
+							where	(i',j') 	= if rs ! i <= rs ! j 
+											then (i,j) 
+											else (j,i)
+								(mi',mi'') 	= unzip $ zipWith3 trasfer (elems qs) (elems $ miq ! i') (elems $ miq ! j')
 
-operatore ::  Bounds -> Limite -> Quanti -> Ranks -> Matrice Interesse -> StdGen -> Elemento (Variazioni Soluzioni) (Matrice Quantità) 
-operatore b l q r mi = Operatore . f  where
-	f s (Soluzione mq) = (operatore b l q r mi (fst . split $ s), second Soluzione $ modifica b l q r mi s mq) 
-		
 soluzione :: Matrice Quantità -> Elemento Soluzioni (Matrice Quantità)
 soluzione = Soluzione
 
-	{-
-	is = shuffle s $ indices rs
-	(js,ks) = splitAt (size rs `div` 2)
-	miq = x
-	-}
+tagliaSoluzioni n xs = take n $ sortBy (comparing (negate . fst)) $ elems xs
+tagliaOperatori = tagliaSoluzioni
 
